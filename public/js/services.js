ce7a814122a74acbf1f0416485d226029292b663
Jayant Gandhi <jayant.gandhi@gmail.com>
2015-01-19 21:38:22 +0100

    start work on edu stuff

--- public/js/services.js
+++ public/js/services.js
@@ -8,7 +8,37 @@ appServices.factory('Site', ['$resource',
         isArray:true,
         transformResponse: function(data, header) {
           var wrappedobj = angular.fromJson(data);
-          return wrappedobj.sites;
+          return wrappedobj.list;
+        }
+      }
+    });
+  }]);
+
+// GET educated
+appServices.factory('Edu', ['$resource',
+  function($resource){
+    return $resource('/api/education', {}, {
+      query: {
+        method:'GET',
+        isArray:true,
+        transformResponse: function(data, header) {
+          var wrappedobj = angular.fromJson(data);
+          return wrappedobj.list;
+        }
+      }
+    });
+  }]);
+
+// GET experience
+appServices.factory('Exp', ['$resource',
+  function($resource){
+    return $resource('/api/experience', {}, {
+      query: {
+        method:'GET',
+        isArray:true,
+        transformResponse: function(data, header) {
+          var wrappedobj = angular.fromJson(data);
+          return wrappedobj.list;
         }
       }
     });
