9e3c5289ce234b18010ef04623e5ef25cdd80ce9
Jayant Gandhi <jayant.gandhi@gmail.com>
2015-11-02 11:25:27 -0500

    breakin it down

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -29,21 +29,3 @@ appControllers.controller('CvController', ['$scope', '$http', 'Edu', 'Exp',
     $scope.experience = Exp.query();
 
 }]);
-
-// appControllers.controller('CollapseController', function ($scope) {
-//   $scope.isCollapsed = true;
-// });
-
-var sites = [
-  {
-    'name' : 'Blue Haven Capital',
-    'url'  : 'http://www.bluehavencapital.com',
-    'dateCreated' : new Date(2014, 8)
-  },
-  {
-    'name' : 'The SAIS Europe Journal of Global Affairs',
-    'url'  : 'http://www.saisjournal.org',
-    'dateCreated' : new Date(2015, 1)
-  }
-
-];
