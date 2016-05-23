7a72824c021e75072c5c3714db733040ed743236
Jayant Gandhi <jayant.gandhi@gmail.com>
2016-05-22 23:55:08 -0700

    swap circles for tabs

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -34,6 +34,25 @@ appControllers.controller('HomeController', ['$scope', '$rootScope', '$location'
     $scope.menuPrompt = function(promptText) {
       $scope.promptText = promptText;
     }
+
+    angular.element(document).ready(function () {
+      (function ($) {
+    		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+
+    		$('.tab ul.tabs li a').click(function (g) {
+    			var tab = $(this).closest('.tab'),
+    				index = $(this).closest('li').index();
+
+    			tab.find('ul.tabs > li').removeClass('current');
+    			$(this).closest('li').addClass('current');
+
+    			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+    			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+
+    			g.preventDefault();
+    		} );
+    	})(jQuery);
+    });
   }
 ]);
 
